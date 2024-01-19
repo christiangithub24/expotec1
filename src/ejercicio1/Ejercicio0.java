51fb3fc66e0c8d2aff13b62ab9271c16257d4045
Christian Morales <progra@gmail.com>
2024-01-19 11:17:29 -0600

    primer repositorio

---
+++ src/ejercicio1/Ejercicio0.java
@@ -0,0 +1,39 @@
+
+package ejercicio1;
+
+public class Ejercicio0 {
+
+    public static void main(String[] args) {
+        //Declaracion de variable int
+        int x;
+        //Asignacion de variable int
+        x = 5;
+        
+        // Declaracion y asignacion de variable int
+        int y = 5;
+        // Declaracion y asignacion de variable String
+        String nombre = "Pepito";
+        
+        // imprimir en consola
+        System.out.println("Hola "+ x+y);
+        int suma = x+y;
+        boolean n;
+        System.out.print("La suma de x + y es igual a: ");
+        System.out.print(suma);
+        
+        
+        
+        // Esto es un comentario de una linea
+        
+        /*
+        Esto
+        es
+        un
+        comentario
+        multilinea        
+        */
+        
+    }
+    
+    
+}
